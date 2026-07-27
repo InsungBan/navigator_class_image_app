ff6e2269b810769f50c4153da0a26dc4e92a6f35
인성 <twapet0124@gmail.com>
2026-07-27 15:25:59 +0900

    home.dart 생성
    home.dart 초기 생성

---
+++ navigator_class_image_app/lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(("Home page")),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        centerTitle: false,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, "/1st"),
+              child: Text("First Page"),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, "/2nd"),
+              child: Text("Second Page"),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, "/3rd"),
+              child: Text("Third Page"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
