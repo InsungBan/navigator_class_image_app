5c798f326dd732f08436ae2611748eca9b7c1b09
minjae0615 <06minjae@naver.com>
2026-07-27 16:53:49 +0900

    123
    123

--- navigator_class_image_app/lib/main.dart
+++ navigator_class_image_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'view/first_page.dart';
-import 'view/home.dart';
+import 'home.dart';
 import 'view/image1.dart';
 
 void main() {
@@ -22,10 +22,10 @@ class MyApp extends StatelessWidget {
         "/" :(context) => Home(),
         "/1st" :(context) => FirstPage(),
         "/image1" :(context) => FirstImagePage(),
-        // "/2nd" :(context) => SecondPage(),
-        // "/image2" :(context) => SecondImagePage(),
-        // "/3th" :(context) => ThirdPage(),
-        // "/image3" :(context) => ThirdImagePage(),
+        "/2nd" :(context) => SecondPage(),
+        "/image2" :(context) => SecondImagePage(),
+        "/3th" :(context) => ThirdPage(),
+        "/image3" :(context) => ThirdImagePage(),
       },
       initialRoute: "/",
     );
